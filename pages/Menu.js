0507ebd69da94a4c8316d26c67513dd04cec5636
parrot <parrot@localhost.localdomain>
2024-02-29 21:59:55 -0800

    Just need to configure page navigation and make home page pretty

---
+++ pages/Menu.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Menu = () => {
+	return (
+		<View style={styles.container}>
+		<Text>Menu Screen</Text>
+		{/* Add your menu components here */}
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+});
+
+export default Menu;
